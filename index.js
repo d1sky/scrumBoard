50787c4f23967586f971137055ab01d966332269
d1sky <godasin@gmail.com>
2022-04-11 07:37:25 +0300

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+// console.log('helloWo');
+
+// document.body.onload = addElement;
+
+// function addElement () {
+//   // create a new div element
+//   const column = document.createElement("div");
+
+//   let div = document.createElement("p")
+
+//   // and give it some content
+//   const newContent = document.createTextNode("Hi there and greetings!");
+
+//   // add the text node to the newly created div
+//   column.appendChild(newContent);
+
+//   // add the newly created element and its content into the DOM
+//   const main = document.getElementById("main");
+
+//   main.appendChild(column);
+// }
+
+
+
+// // var element = document.getElementById('main');
+// // var column = document.createElement('div');
+// // element.class = 'column';
+// // element.innerText = 'helloWo'
